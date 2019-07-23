4750ad070747b3ce29b8f2374fa5b25a726774c3
Akash Sawant <sawantakash321@gmail.com>
2019-07-23 17:38:51 +0530

    Initial commit

---
+++ app/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.scss';
+
+const Footer = () => (
+  <footer>
+    <section>This project is licensed under the MIT license.</section>
+    <section>
+      Made with{' '}
+      <span role="img" aria-label="heart-emoji">
+        ❤️
+      </span>{' '}
+      by <a href="" />
+    </section>
+  </footer>
+);
+
+export default Footer;
